b388de40f3a4e930e42aa5e1f3648221aa3e072e
GaliSagiv <galisagiv5@gmail.com>
2025-07-03 14:50:56 +0300

    added a sign up page

--- src/app.js
+++ src/app.js
@@ -6,13 +6,21 @@ const notificationRoutes = require('./routes/notification.routes');
 
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/notifications', notificationRoutes);
+app.use(express.static('src/public'));
 
+app.get('/signup', (req, res) => {
+  res.render('signup'); 
+});
 
 app.get('/', (req, res) => {
     res.send('API is running...');
