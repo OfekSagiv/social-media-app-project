750e8ecc86c900b2d98512284fccf10480ef8517
OfekSagiv <ofeksagiv@gmail.com>
2025-07-04 20:37:04 +0300

    extract routes into view.routes

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const postRoutes = require('./routes/post.routes');
 const groupRoutes = require('./routes/group.routes');
 const notificationRoutes = require('./routes/notification.routes');
 const authRoutes = require('./routes/auth.routes');
+const viewRoutes = require('./routes/view.routes');
 const { sessionMiddleware } = require('./middleware/auth');
-const { isLoggedIn } = require('./middleware/auth');
 
 const app = express();
 
@@ -17,36 +17,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(sessionMiddleware);
 app.use(express.json());
 
+app.use('/', viewRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/notifications', notificationRoutes);
-app.use(express.static('src/public'));
-
-app.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-app.use('/api/auth', authRoutes);
-
-app.get('/', (req, res) => {
-    const user = req.session?.user;
-    if (user) {
-        return res.redirect('/home');
-    }
-    res.redirect('/login');
-});
-
-app.get('/home', isLoggedIn, (req, res) => {
-    const user = req.session.user;
-    res.render('home', {
-        fullName: user.fullName,
-        user
-    });
-});
 
 module.exports = app;
 
